15a7dda3f9a2b6dcc32e206bf0108739fc6ad429
Alex <alex.thepaut33@gmail.com>
2019-07-31 22:33:31 +0200

    Geolocation service, Pop up catalogue object

--- src/app/pibble-connection/pibble-connection.component.ts
+++ src/app/pibble-connection/pibble-connection.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { SocketService } from '../services/socket.service';
 import { Router } from '@angular/router';
 import { PATH_RACKET } from '../app.constantes';
+import { GeolocalisationService } from '../services/geolocalisation.service';
 
 @Component({
   selector: 'app-pibble-connection',
@@ -11,23 +10,18 @@ import { PATH_RACKET } from '../app.constantes';
 })
 export class PibbleConnectionComponent implements OnInit {
 
-  ipCtrl: FormControl;
-  portCtrl: FormControl;
-  connectForm: FormGroup;
-
-  constructor(private fb: FormBuilder, private socketService: SocketService, private router: Router) {
-    this.ipCtrl = fb.control('', [Validators.required, Validators.pattern(/^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])/)]);
-    this.portCtrl = fb.control('', [Validators.required]);
-    this.connectForm = fb.group({
-      ipv4: this.ipCtrl,
-      port: this.portCtrl
-    });
+  constructor(private router: Router, private geolocation: GeolocalisationService) {
   }
 
   ngOnInit() {
   }
 
   handleSubmit() {
+    this.geolocation.getCurrentPosition().subscribe(
+      data => {
+        console.log(data);
+      }
+    )
     this.router.navigate([PATH_RACKET]);
   }
 
---
+++ src/app/services/geolocalisation.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Observable, Observer } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GeolocalisationService {;
+
+  constructor() { }
+
+  getCurrentPosition(): Observable<Position> {
+    return Observable.create((observer: Observer<Position>) => {
+        // Invokes getCurrentPosition method of Geolocation API.
+        navigator.geolocation.getCurrentPosition(
+            (position: Position) => {
+                observer.next(position);
+                observer.complete();
+            },
+            (error: PositionError) => {
+                console.log('Geolocation service: ' + error.message);
+                observer.error(error);
+            }
+        );
+    });
+}
+}
