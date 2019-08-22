0e0a6a09282dae147684c73f288727990bcbb45c
Alex <alex.thepaut33@gmail.com>
2019-08-22 18:37:56 +0200

    Add ssl certificate

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 const express = require('express');
-const http = require('http');
+const https = require('https');
 const path = require('path');
+const fs = require('fs');
+
+var key = fs.readFileSync(__dirname + '/certificates/selfsigned.key');
+var cert = fs.readFileSync(__dirname + '/certificates/selfsigned.crt');
+
+var credentials = {key: key, cert: cert};
 
 const app = express();
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 8001;
 
 app.use(express.static(__dirname + '/dist/pibbleApp'));
 
 app.get('/*', (req, rep) => res.sendFile(path.join(__dirname)));
 
-const server = http.createServer(app);
+const server = https.createServer(credentials, app);
 
 server.listen(port, () => console.log('Running ...'));
